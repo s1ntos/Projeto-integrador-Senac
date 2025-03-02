ad94e1b19badd023006fa5e918cd74f11b189eed
João <109487647+s1ntos@users.noreply.github.com>
2025-03-01 22:30:19 -0300

    Update ConexaoBD.java

--- atv3upi/atv2uu/src/java/ConexaoBD.java
+++ atv3upi/atv2uu/src/java/ConexaoBD.java
@@ -3,9 +3,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoBD {
-    private static final String URL = "jdbc:mysql://localhost:3306/atv3pi";
+    private static final String URL = "jdbc:mysql://localhost:3306/nome-bancodedados"; //nome do banco de dados
     private static final String USER = "root";
-    private static final String PASSWORD = "joaocosta34";
+    private static final String PASSWORD = "password"; //coloque sua senha
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
